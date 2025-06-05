2505b9a8c7b726e19cf9d8942e2e58696ebb4c4e
João Schweitzer <142324868+J-Schweitzer@users.noreply.github.com>
2025-06-04 22:14:47 -0400

    Add files via upload

---
+++ src/Modelo/UsuarioM.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author ra189395
+ */
+public class UsuarioM {
+    private int id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String senha;
+
+    
+    
+    
+}
